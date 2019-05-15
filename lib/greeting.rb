09532806f637818297e80bae93ac6a0ea69fa9f0
Stephen Wool <stevewool7@gmail.com>
2019-05-15 20:55:03 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,4 +1,4 @@
 def greeting(name)
-  puts "Hello #{name}. It's nice to meet you.".split.join("")
+  puts "Hello #{name}. It's nice to meet you.".split.join(" ")
 end
   # code the #greeting method here!
